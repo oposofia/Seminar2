2297778fd079c6934992985aa1577a6a9227f4bb
OpoSofia <sko88@bk.ru>
2023-09-24 18:56:52 +0400

    Task 15 Homework

---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool day = DayOfWeek(number);
+bool result = Weekand(number);
+if (day == true) Console.WriteLine ("Некорректный ввод");
+else Console.WriteLine(result ? "это выходной" : "это не выходной");
+
+bool DayOfWeek (int num)
+{
+    if ((num<1) || (num>7)) return true;
+    return false;
+}
+
+bool Weekand (int num)
+{
+    if ((num == 6) || (num == 7)) return true;
+    else return false;
+}
